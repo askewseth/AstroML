a579ac08807839e3f559ed48be1a0efc60d329ec
Seth <askewseth@gmail.com>
2016-03-07 16:00:47 -0500

    Added progress bar for getSpecs

--- functions.py
+++ functions.py
@@ -2,6 +2,7 @@ import os
 from Spectrum import spectrum
 import SimbadSearch as ss
 import math
+from threading import Thread
 
 
 def getSpecs(path = '/home/oort/Downloads/AstroFilesRaw/AstroFiles/', nots=False):
@@ -10,7 +11,8 @@ def getSpecs(path = '/home/oort/Downloads/AstroFilesRaw/AstroFiles/', nots=False
     nots_arr = []
     filenames = [path + x for x in os.listdir(path) if '.fits' in x]
     print 'length filenames: ', len(filenames)
-    for x in filenames:
+    for i, x in enumerate(filenames):
+        print i, '\r',
         try:
             specs.append(spectrum(x))
         except Exception as e:
@@ -43,3 +45,28 @@ def getCors(path='/home/oort/Downloads/AstroFilesRaw/AstroFiles/'):
             nots_arr.append(x)
     print 'length specs: ', len(specs), '\tlength errors: ', len(errors)
     return dic, errors
+
+
+def getThread(path='/home/oort/Downloads/AstroFilesRaw/AstroFiles/'):
+    names = []
+    def convert(x):
+        try:
+            s = spectrum(x)
+            s.convertCSVNew()
+            names.append(s.csv_name)
+            s.delete()
+        except Exception as e:
+            print e
+    threads = []
+    filenames = [path + x for x in os.listdir(path) if '.fits' in x]
+    for i, x in enumerate(filenames):
+        try:
+            t = Thread(target=convert, args=(x,))
+            threads.append(t)
+            t.start()
+        except:
+            print 'ERROR'
+    # print 'length specs: ', len(specs), '\tlength errors: ', len(errors)
+    # return dic, errors
+    print 'DONE!'
+    return names
