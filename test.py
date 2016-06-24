886591fe6b2034a6ddeeff94fc53f612fd6b73c8
Seth <askewseth@gmail.com>
2016-06-24 17:27:15 -0400

    Added some tests.

--- test.py
+++ test.py
@@ -1,4 +1,16 @@
 import tests
+from Spectrum import spectrum
 
 print len(tests.file_paths)
 print 'DONE'
+
+
+def catch(func, *args, **kwargs):
+    try:
+        return func(*args, **kwargs)
+    except Exception as e:
+        print "EXCEPTION:"
+        print '\t', e
+        print '\t', ','.join(args)
+
+specs = [catch(spectrum, f) for f in tests.file_paths]
--- tests/testSpec.py
+++ tests/testSpec.py
@@ -6,4 +6,13 @@ script_dir = os.path.dirname(__file__)
 rel_path = 'files/'
 abs_file_path = os.path.join(script_dir, rel_path)
 
-print 'DONE'
+
+def catch(func, *args, **kwargs):
+    try:
+        return func(*args, **kwargs)
+    except Exception as e:
+        print "EXCEPTION:"
+        print '\t', e
+        print '\t', ','.join(args)
+
+specs = [catch(spectrum, x) for x in file_paths]
